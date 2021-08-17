05b3a05adfeb1b372a4a84239a592a4449c7b2a2
rgv3300 <rgv3300@gmail.com>
2021-08-17 09:40:25 -0400

    adding configuration

--- src/ToDoAPI/Startup.cs
+++ src/ToDoAPI/Startup.cs
@@ -8,18 +8,28 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ToDoAPI.Data;
+using Microsoft.Extensions.Configuration;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 
 namespace ToDoAPI
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string DBConnection = _configuration.GetConnectionString("DefaultConnection");
             services.AddControllers();
-            services.AddDbContext<TaskContext>();
+            services.AddDbContext<TaskContext>(options => options.UseNpgsql(DBConnection));
             services.AddScoped<ITaskRepo, TaskRepo>();
 
         }
