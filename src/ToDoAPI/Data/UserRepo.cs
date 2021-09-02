15e264d20f24817023913dce6756c1c028832176
rgv3300 <rgv3300@gmail.com>
2021-09-02 15:09:04 -0400

    user manager added

--- src/ToDoAPI/Data/UserRepo.cs
+++ src/ToDoAPI/Data/UserRepo.cs
@@ -2,6 +2,7 @@ using ToDoAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace ToDoAPI.Data
 {
@@ -15,10 +16,17 @@ namespace ToDoAPI.Data
             this.userManager = userManager;
             this.signInManager = signInManager;
         }
-        public async Task<IdentityResult> LoginUser(TaskUser taskUser)
+        public async Task<IdentityResult> LoginUserAsync(TaskUser taskUser)
         {
             var user = new IdentityUser { UserName = taskUser.Email, Email = taskUser.Email };
             var result = await userManager.CreateAsync(user, taskUser.Password);
+
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("api/Tasks");
+            }
+
         }
     }
 }
